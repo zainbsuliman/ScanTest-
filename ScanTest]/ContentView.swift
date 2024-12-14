61b5fe600535d2a033f3dbc8518ee2269d07dfa7
zainbsuliman <zat301099@gmail.com>
2024-12-14 17:10:00 +0300

    Initial Commit

---
+++ ScanTest]/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScanTest]
+//
+//  Created by Zainab Alatwi  on 13/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScanTest]/ScanTest_App.swift
@@ -0,0 +1,17 @@
+//
+//  ScanTest_App.swift
+//  ScanTest]
+//
+//  Created by Zainab Alatwi  on 13/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ScanTest_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
